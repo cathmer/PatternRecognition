97972cefeeca660f4d1c2ff8ac0ad610afa16be8
Casper <c.athmer@student.tudelft.nl>
2018-12-24 11:37:12 +0100

    Added some preprocessing

--- main.m
+++ main.m
@@ -1,3 +1,21 @@
-setup
-a = prnist([0:9],[1:40:1000]);
-show(a);
+setup; % This file should load the paths to the course files
+prwaitbar off;
+
+a = prnist([0:9],[1:100:1000]);
+%figure(1); show(a);
+
+b = preprocessing(prnist([0:9],[1:100:1000]));
+%figure(2); show(b);
+
+w = svc(b); % Trains a support vector machine
+labels = b*w*labeld; % Shows the labels that the classifier assigns to objects from b
+
+% Evaluate the performance of the classifier
+e = nist_eval('preprocessing', w, 10);
+disp(e);
+
+%Uncomment this to see how rotation influences the images
+%rotation(a);
+
+%pixelclassification(a);
+
---
+++ preprocessing.m
@@ -0,0 +1,8 @@
+% Preprocessing function used for pixel classification
+function a = preprocessing(m)
+
+% Preprocessing to make all the images square, resize them to 30x30 pixels
+% and put an empty box around them. 
+preproc = im_box([],0,1)*im_resize([], [20 20])*im_box([],1,0);
+m = m * preproc;
+a = prdataset(m);
