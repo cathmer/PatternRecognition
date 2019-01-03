01df64317248934fe0a78ba582c80a61f8947f60
Casper <c.athmer@student.tudelft.nl>
2019-01-03 10:46:47 +0100

    Few changes

--- main.m
+++ main.m
@@ -1,17 +1,17 @@
 setup; % This file should load the paths to the course files
-prwaitbar off;
-
-a = prnist([0:9],[1:100:1000]);
-%figure(1); show(a);
+clear all;
+%prwaitbar off;
 
 b = preprocessing(prnist([0:9],[1:100:1000]));
 %figure(2); show(b);
+disp('Finished preprocessing b!');
 
-w = svc(b); % Trains a support vector machine
+w = b*svc(proxm('p',5)); % Trains a support vector machine
 labels = b*w*labeld; % Shows the labels that the classifier assigns to objects from b
+disp('Finished training b');
 
 % Evaluate the performance of the classifier
-e = nist_eval('preprocessing', w, 10);
+e = nist_eval('preprocessing', w, 100);
 disp(e);
 
 %Uncomment this to see how rotation influences the images
--- preprocessing.m
+++ preprocessing.m
@@ -3,6 +3,6 @@ function a = preprocessing(m)
 
 % Preprocessing to make all the images square, resize them to 30x30 pixels
 % and put an empty box around them. 
-preproc = im_box([],0,1)*im_resize([], [20 20])*im_box([],1,0);
+preproc = im_box([],0,1)*im_resize([], [28 28])*im_box([],1,0);
 m = m * preproc;
 a = prdataset(m);
